f5f93ea4e14662acca8f90e2e93f799d57758e17
Alfred Brenda <itzel.sanchez.biss@gmail.com>
2018-04-24 21:31:27 -0500

    Creación de la solución

---
+++ Licenciamiento_CIEA/src/licenciamiento_ciea/Licenciamiento_CIEA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package licenciamiento_ciea;
+
+/**
+ *
+ * @author Sal
+ */
+public class Licenciamiento_CIEA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
